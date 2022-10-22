0f6f7a9973f73b1e2c4affb18639b139f6b6e3bb
Lukix01 <e@lklos.com>
2022-10-23 00:44:28 +0200

    count lines

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,28 @@
+import fs from 'node:fs';
+import glob from 'glob';
+
 export default function Manylines(): void {
-  console.log('test');
+  const args: string[] = process.argv.slice(2);
+  const path = './test';
+  const fileList: any[] = [];
+  let linesOfCode: number = 0;
+
+  glob(path + '/**/*+(.tsx|.ts|.jsx|.js|.css)', { ignore: [ path + '/node_modules/**/*' ] }, (er, files): void => {
+    for (const file of files) {
+      if (!fs.lstatSync(file).isDirectory()) {
+        const data: string = fs.readFileSync(file, { encoding: 'utf8', flag: 'r' });
+        const lines: number = data.split('\n').length;
+        fileList.push({ file, lines });
+        linesOfCode += lines;
+      }
+    }
+
+    if (args[1] === '--files') {
+      console.log(fileList, 'All lines of code:', linesOfCode);
+    } else {
+      console.log('All lines of code:', linesOfCode);
+    }
+  });
 }
 
 Manylines();
